1de0091c67734de046d525490f5557f5e9a35128
Brock Friedrich <brocklfriedrich@gmail.com>
2020-08-04 21:25:36 -0500

    refactored aggregate proxy

--- src/sunstruck/db/models/bases.py
+++ src/sunstruck/db/models/bases.py
@@ -10,6 +10,8 @@ from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.dialects.postgresql.dml import Insert
 from sqlalchemy.schema import Constraint, PrimaryKeyConstraint
 from sqlalchemy.sql.base import ImmutableColumnCollection
+from sqlalchemy.sql.elements import TextClause
+from sqlalchemy.sql.functions import Function
 from sqlalchemy_utils import ChoiceType, EmailType, URLType
 
 import util
@@ -236,29 +238,56 @@ class AggregateProxy:
     def default_column(self) -> Column:
         return self._pk[0] if len(list(self._pk)) > 0 else self._c[0]
 
-    async def count(self) -> int:
-        return await db.func.count(self.default_column).gino.scalar()
+    def ensure_column(self, column: Union[str, Column] = None) -> Column:
+        if isinstance(column, str):
+            column = self._c[column]
+        elif column is None:
+            column = self.default_column
+        return column
 
-    async def max(self, column: Union[str, Column] = None) -> int:
+    # TODO: test with and without filter
+    async def agg(
+        self,
+        funcs: Union[Function, List[Function]],
+        filter: Union[str, TextClause] = None,
+    ) -> Dict[str, Union[int, float]]:
+        func_map: Dict[str, Function] = {f.name: f for f in util.ensure_list(funcs)}
+
+        q = db.select(func_map.values())
+
+        if filter is not None:
+            if not isinstance(filter, TextClause):
+                filter = db.text(filter)
+            q = q.where(filter)
+
+        results: List = await q.gino.one()
+
+        return dict(zip(func_map, results))
+
+    async def count(self, filter: Union[str, TextClause] = None) -> int:
+        """ Get the model's rowcount """
+
+        result = await self.agg(db.func.count(self.default_column), filter=filter)
+        return util.reduce(result.values())
+
+    async def max(
+        self, column: Union[str, Column] = None, filter: Union[str, TextClause] = None
+    ) -> int:
         """ Get the maximum value of the given column.  If no column is specified,
             the max value of the first primary key column is returned.
         """
 
-        if isinstance(column, str):
-            column = self._c[column]
-        elif column is None:
-            column = self.default_column
+        func: Function = db.func.max(self.ensure_column(column))
+        result = await self.agg(func, filter=filter)
+        return util.reduce(result.values())
 
-        return await db.func.max(column).gino.scalar()
-
-    async def min(self, column: Union[str, Column] = None) -> int:
+    async def min(
+        self, column: Union[str, Column] = None, filter: Union[str, TextClause] = None
+    ) -> int:
         """ Get the minimum value of the given column.  If no column is specified,
             the min value of the first primary key column is returned.
         """
 
-        if isinstance(column, str):
-            column = self._c[column]
-        elif column is None:
-            column = self.default_column
-
-        return await db.func.min(column).gino.scalar()
+        func: Function = db.func.min(self.ensure_column(column))
+        result = await self.agg(func, filter=filter)
+        return util.reduce(result.values())
