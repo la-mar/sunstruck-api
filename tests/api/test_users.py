55042c870f2e5cc5f417790a4fac7a0cdf3ad618
Brock Friedrich <brocklfriedrich@gmail.com>
2020-08-07 07:47:46 -0500

    user endpoint tests

--- tests/api/test_users.py
+++ tests/api/test_users.py
@@ -3,6 +3,7 @@ import logging
 import pytest
 import starlette.status as codes
 
+import config as conf
 from api.v1.endpoints.users import ERROR_404
 from db.models import User as Model
 from tests.utils import rand_email, rand_str, seed_model
@@ -29,10 +30,13 @@ def user():
     }
 
 
-async def test_read_me(client, superuser_token_headers):
+async def test_read_me(authorized_client):
 
-    response = await client.get(f"{path}/me", headers=superuser_token_headers)
-    logger.warning(response.json())
+    response = await authorized_client.get(f"{path}/me")
+    assert response.status_code == codes.HTTP_200_OK
+
+    user_data = response.json()
+    assert user_data["username"] == conf.MASTER_USERNAME
 
 
 async def test_create_user(client, user):
@@ -46,6 +50,7 @@ async def test_list_users(client):
     expected_record_count = 25
     response = await client.get(path)
     assert response.status_code == codes.HTTP_200_OK
+
     data = response.json()
     assert len(data) == expected_record_count
     assert response.links["next"] is not None
@@ -55,6 +60,7 @@ async def test_get_user(client):
     id = 20
     response = await client.get(f"{path}/{id}")
     assert response.status_code == codes.HTTP_200_OK
+
     data = response.json()
     assert data["id"] == 20
 
@@ -63,6 +69,7 @@ async def test_update_exising_user(client, user):
     id = 10
     response = await client.put(f"{path}/{id}", json=user)
     assert response.status_code == codes.HTTP_200_OK
+
     data = response.json()
     assert data["id"] == id
     assert data["username"] == user["username"]
@@ -72,6 +79,7 @@ async def test_partial_update_exising_user(client, user):
     id = 10
     response = await client.patch(f"{path}/{id}", json=user)
     assert response.status_code == codes.HTTP_200_OK
+
     data = response.json()
     assert data["id"] == id
     assert data["username"] == user["username"]
@@ -87,6 +95,7 @@ async def test_delete_existing_user(client):
     id = 20
     response = await client.delete(f"{path}/{id}")
     assert response.status_code == codes.HTTP_200_OK
+
     data = response.json()
     assert data["id"] == id
 
@@ -95,5 +104,6 @@ async def test_delete_user_not_found(client):
     id = 99999
     response = await client.delete(f"{path}/{id}")
     assert response.status_code == codes.HTTP_404_NOT_FOUND
+
     data = response.json()
     assert data["detail"] == ERROR_404["detail"]
