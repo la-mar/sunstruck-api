ab7db91ed064cd8e6bf7630208ba011a3695b5d4
Brock Friedrich <brocklfriedrich@gmail.com>
2020-08-09 16:46:18 -0500

    add oauth2 client credentials flow

--- src/sunstruck/api/helpers/auth.py
+++ src/sunstruck/api/helpers/auth.py
@@ -1,5 +1,7 @@
-from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+from typing import Optional
+
+from fastapi import Depends, HTTPException, Request, status
+from fastapi.security import oauth2
 from jose import jwt
 from pydantic import ValidationError
 
@@ -9,7 +11,7 @@ from config import API_V1
 from db.models import User
 from schemas import TokenPayload
 
-oauth2_password = OAuth2PasswordBearer(tokenUrl=f"{API_V1}/login/access-token")
+oauth2_password = oauth2.OAuth2PasswordBearer(tokenUrl=f"{API_V1}/login/access-token")
 
 
 async def get_current_user(token: str = Depends(oauth2_password)) -> User:
@@ -45,3 +47,35 @@ def get_current_active_superuser(
             status_code=400, detail="The user doesn't have enough privileges"
         )
     return current_user
+
+
+class Oauth2ClientCredentials(oauth2.OAuth2):
+    def __init__(
+        self,
+        tokenUrl: str,
+        scheme_name: str = None,
+        scopes: dict = None,
+        auto_error: bool = True,
+    ):
+
+        flows = oauth2.OAuthFlowsModel(
+            clientCredentials={"tokenUrl": tokenUrl, "scopes": scopes or {}}
+        )
+        super().__init__(flows=flows, scheme_name=scheme_name, auto_error=auto_error)
+
+    async def __call__(self, request: Request) -> Optional[str]:
+        authorization: str = request.headers.get("Authorization")
+        scheme, param = oauth2.get_authorization_scheme_param(authorization)
+        if not authorization or scheme.lower() != "bearer":
+            if self.auto_error:
+                raise HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                    detail="Not authenticated",
+                    headers={"WWW-Authenticate": "Bearer"},
+                )
+            else:
+                return None
+        return param
+
+
+oauth2_client_credentials = Oauth2ClientCredentials(tokenUrl=f"{API_V1}/login/???????")
