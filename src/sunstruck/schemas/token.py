748dabf69c40e8998186b7c1eb2b6cc45ffec15d
Brock Friedrich <brocklfriedrich@gmail.com>
2020-08-10 18:48:05 -0500

    added details to token schemas

--- src/sunstruck/schemas/token.py
+++ src/sunstruck/schemas/token.py
@@ -1,14 +1,17 @@
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 __all__ = ["Token", "TokenPayload"]
 
 
 class Token(BaseModel):
     access_token: str
-    token_type: str
+    token_type: str = Field("Bearer", example="Bearer")
 
 
 class TokenPayload(BaseModel):
-    sub: Optional[int] = None
+    sub: Optional[int] = Field(
+        None,
+        description="Subject, or purpose, of the token. For example, a url or user's email. ",
+    )
