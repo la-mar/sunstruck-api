d0173d6eda16726a3854365fc9ea7a0190438116
Brock Friedrich <brocklfriedrich@gmail.com>
2020-08-14 19:06:03 -0500

    refining filter parser

--- src/sunstruck/api/helpers/filtering.py
+++ src/sunstruck/api/helpers/filtering.py
@@ -1,10 +1,16 @@
-import itertools
+import logging
 import re
-from urllib import parse as urlparse
+from typing import Any, Callable, Dict, List, Optional
 
-import pandas as pd
+from pydantic import parse_obj_as
+from sqlalchemy import and_
+from sqlalchemy.sql.elements import BinaryExpression, ClauseList
+from sqlalchemy.sql.schema import Column
 
-urlparse.parse_qs("filter=field:ge:7:lt:10")
+from db.models import Model
+from schemas.query_filter import FilterParam, FilterParams
+
+logger = logging.getLogger(__name__)
 
 regex = r"""
         # (?:\?filter=)?
@@ -12,7 +18,7 @@ regex = r"""
         (?P<field_name>[:|]?\w+[^:|'\"])?
         (?P<sep>[:|])
         (?P<inverter>~)?
-        (?P<operator>eq|gte|gt|lte|lt|in|between|regex|like)
+        (?P<operator>eq|gte|gt|lte|lt|in|between|regex|like|is})
         (?=:(?P<value>
         (?P<quoted>['\"][^'\"\\]*(?:\\.[^'\\]*)*['\"])
         |
@@ -20,87 +26,147 @@ regex = r"""
         ))
         """
 
-pattern = re.compile(regex, re.VERBOSE)
+PATTERN = re.compile(regex, re.VERBOSE)
 
-compound = [
-    '?filter=field:gte:7|other_field:~in:a,b,c:another_field:between:"2020-01-01T17:22:20.937752,2020-01-31T17:22:20.937752":yet_another_field:eq:2020'  # noqa
-]
-
-equality = [
-    "?filter=field:eq:7",
-    '?filter=field:eq:"help"',
-    "?filter=field:~eq:7",
-    '?filter=field:~eq:"help"',
-]
-
-inequality = [
-    "?filter=field:gt:7",
-    # "?filter=field:lt:2020-01-01T17\\:22\\:20.937752",
-    "?filter=field:gte:7",
-    "?filter=field:gt:7|lt:14",
-    "?filter=field:gte:7:lte:14",
-    "?filter=field:gt:7:other_field:lt:10",
-    "?filter=field:gte:7|other_field:lte:10",
-    "?filter=field:gt:7|lt:14:other_field:gt:50:another_field:eq:100",
-]
-
-membership = [
-    "?filter=field:in:a,b,c",
-    "?filter=field:in:a,b,c:in:1,2,3",
-    "?filter=field:in:a,b,c|in:1,2,3",
-    "?filter=field:in:a,b,c:other_field:in:a,b,c",
-    "?filter=field:in:a,b,c|other_field:in:1,2,3",
-    "?filter=field:~in:a,b,c",
-    "?filter=field:~in:a,b,c:~in:1,2,3",
-    "?filter=field:~in:a,b,c|~in:1,2,3",
-    "?filter=field:~in:a,b,c:other_field:~in:a,b,c",
-    "?filter=field:~in:a,b,c|other_field:~in:1,2,3",
-    "?filter=field:between:2020-01-01,2020-01-31",
-    '?filter=field:between:"2020-01-01T17:22:20.937752,2020-01-31T17:22:20.937752"',
-    "?filter=field:between:1,10",
-    "?filter=field:between:1,10:other_field:between:100,200",
-]
-
-similarity = [
-    '?filter=field:regex:"?.*val-(.*)"',
-    '?filter=field:regex:"?.*val-(.*)":regex:".*"',
-    '?filter=field:regex:"?.*val-(.*)"|regex:".*"',
-    '?filter=field:regex:"?.*val-(.*)":other_field:regex:".*"',
-    '?filter=field:regex:"?.*val-(.*)"|other_field:regex:".*"',
-    '?filter=field:like:"*bueno"',
-    '?filter=field:like:"mui:bueno"',
-    '?filter=field:like:"*bueno"|like:"*bueno"',
-    '?filter=field:like:"*bueno":other_field:like:"*bueno"',
-    '?filter=field:like:"*bueno"|other_field:like:"*bueno"',
-    '?filter=field:like:"*mui:bueno":other_field:like:"*bueno"',
-    '?filter=field:like:"*mui|bueno"|other_field:like:"*bueno"',
-]
-
-strings = compound + equality + inequality + membership + similarity
-records = []
+OPERATIONS: Dict[str, Callable[[Column, Any], BinaryExpression]] = {
+    "is": lambda column, value: column.is_(value),
+    "eq": lambda column, value: column == value,
+    "gt": lambda column, value: column > value,
+    "gte": lambda column, value: column >= value,
+    "lt": lambda column, value: column < value,
+    "lte": lambda column, value: column <= value,
+    "like": lambda column, value: column.like(value),
+    "in": lambda column, value: column.in_(value),
+    "between": lambda column, value: column.between(*value),
+    "~is": lambda column, value: column.isnot_(value),
+    "~eq": lambda column, value: column != value,
+    "~like": lambda column, value: column.notlike(value),
+    "~in": lambda column, value: column.notin_(value),
+}
 
 
-matches = list(itertools.chain(*[pattern.findall(s) for s in strings]))
+class Filter:
+    # Dependency for injection
+
+    def __init__(self, model: Model):
+        self.model = model
+        self.column_map = self.model.c.dict()
+
+    def __call__(self, filter: str) -> ClauseList:
+        return self.translate(self.parse(filter))
+
+    @staticmethod
+    def parse(s: str) -> List[FilterParam]:
+        records = [dict(zip(PATTERN.groupindex, m)) for m in PATTERN.findall(s)]
+        return FilterParams(params=records).params
+
+    def translate(self, filters: List[FilterParam]) -> ClauseList:
+        """ Translate the given list of filters to a sqlalchemy predicate.
+
+        Parameters
+        ----------
+        filters : List[FilterParam]
+            list of filter parameter objects
+
+        Returns
+        -------
+        sqlalchemy.elements.ClauseList
+            completed predicate, ready to be passed to
+            sqlalchemy (e.g. .filter(predicate)) or
+            gino (e.g. .where(predicate))
+        """
+
+        group_conjunctive = and_
+        expressions = []
+        field_name: Optional[str] = None
+        expression_group: List[BinaryExpression] = []
+        for filt in filters:
+            if filt.field_name is not None:
+
+                # start of new filter
+                if filt.field_name != field_name:
+
+                    # progressed to another field's filter
+                    if len(expression_group) > 0:
+                        expressions.append(group_conjunctive(*expression_group))
+
+                    expression_group = []
+
+                # field_name changed. update it's ref
+                field_name = filt.field_name
+
+            else:  # field_name is None
+                # filter is a chained filter on the same field, so
+                # carry the previous field name forward
+                pass
+
+            if filt.conjunctive is not None:
+                if filt.conjunctive != group_conjunctive:
+                    if len(expression_group) > 0:
+                        expressions.append(group_conjunctive(*expression_group))
+                    expression_group = []
+
+                group_conjunctive = filt.conjunctive
+                logger.debug(f"group={group_conjunctive.__name__}")
+
+            sep = filt.conjunctive or filt.sep
+            if filt.sep is not None:
+                # if sep != conjunctive:
+                #     if len(expression_group) > 0:
+                #         expressions.append(conjunctive(*expression_group))
+                #     expression_group = []
+
+                # conjunctive changed. update it's ref
+                group_conjunctive = sep
+                logger.debug(f"{group_conjunctive.__name__}")
+
+            column = self.column_map[field_name]
+
+            if filt.operator in ["in", "like", "between"]:
+                # parse as list of items
+                value = [
+                    parse_obj_as(column.type.python_type, v)
+                    for v in filt.value.strip('"').split(",")
+                ]
+            else:
+                # parse as scalar item
+                value = parse_obj_as(column.type.python_type, filt.value)
+
+            op = f"{filt.inverter or ''}{filt.operator}"
+            expression: BinaryExpression = OPERATIONS[op](column, value)
+            expression_group.append(filt.sep(expression))
+            logger.debug([str(x) for x in expression_group])
+
+        # capture remainder
+        if len(expression_group) > 0:
+            expressions.append(group_conjunctive(*expression_group))
+
+        # completed predicate, ready to be passed to
+        # sqlalchemy (e.g. .filter(predicate)) or
+        # gino (e.g. .where(predicate))
+        predicate = and_(*expressions)
+        logger.debug(f"translated filter to sql predicate: {predicate.compile()}")
+        return predicate
 
 
-records = [dict(zip(pattern.groupindex, m)) for m in matches]
+if __name__ == "__main__":
+    s = 'id:lte:5:gte:4|lt:6|is_superuser:~in:false,false|is:true:updated_at:between:"1929-01-01T17:22:20.937752,2020-01-31T17:22:20.937752"'  # noqa
 
-# record = [{k: v for k, v in m.items() if v} for m in mapped]
-# records += record
+    from db import db
+    from db.models import User
 
+    async def wrapper():
 
-records[0]
+        await db.startup()
 
+        filter = Filter(User)
+        # self = filter
 
-pd.options.display.max_rows = 100
-pd.options.display.max_columns = 0
-pd.options.display.expand_frame_repr = True
-pd.options.display.large_repr = "truncate"
-pd.options.display.float_format = lambda x: "%.2f" % x
-pd.options.display.precision = 2
-pd.options.display.max_colwidth = 100
+        filters = filter.parse(s)
+        predicate = filter.translate(filters)
 
-pd.DataFrame(records)
+        # dir(db)
+        # dir(User.id)
+        # predicate = db.and_(User.id.in_([1, 2, 3]))
 
-
-# if there is no command, field_or_value should always be a field name
+        await User.query.where(predicate).gino.all()
