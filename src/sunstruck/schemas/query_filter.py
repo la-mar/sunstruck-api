7f959adb942c654fb8d51015ee2af33d877fdc36
Brock Friedrich <brocklfriedrich@gmail.com>
2020-08-14 19:08:40 -0500

    refine query filter schema

--- src/sunstruck/schemas/query_filter.py
+++ src/sunstruck/schemas/query_filter.py
@@ -1,22 +1,36 @@
-from typing import Dict, List, Optional
+from typing import Callable, Dict, List, Optional
 
-from pydantic import root_validator
+from pydantic import root_validator, validator
 from pydantic.fields import ModelField
+from sqlalchemy import and_, or_
 
+from const import FilterOperator
 from schemas.bases import BaseModel
 
 
 class FilterParam(BaseModel):
-    conjunctive: Optional[str]
-    field_name: str
-    sep: str
+    conjunctive: Optional[Callable]
+    field_name: Optional[str]
+    sep: Optional[Callable]
     inverter: Optional[str]
-    operator: str
+    operator: FilterOperator
     value: str
 
     @root_validator(pre=True)
     def empty_strings_to_none(cls, values):
         return {k: v if v != "" else None for k, v in values.items()}
+
+    @validator("conjunctive", "sep", pre=True)
+    def to_callable(cls, v: str) -> Optional[Callable]:
+        if v:
+            if v == ":":
+                return and_
+            elif v == "|":
+                return or_
+            else:
+                raise ValueError(f"Conjunctive must be one of [':','|'], not {v}")
+
+        return None
 
 
 class FilterParams(BaseModel):
