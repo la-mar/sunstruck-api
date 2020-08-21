018ef89d3963449f6781de22ae050332f2adcb74
Brock Friedrich <brocklfriedrich@gmail.com>
2020-08-21 10:54:22 -0500

    move sqlalchemy props to instance attributes

--- src/sunstruck/db/__init__.py
+++ src/sunstruck/db/__init__.py
@@ -7,6 +7,7 @@ import logging
 from typing import Optional, Type, Union
 
 import sqlalchemy as sa
+from sqlalchemy.engine import Row  # type: ignore
 from sqlalchemy.engine.url import URL
 from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
 from sqlalchemy.ext.asyncio.session import AsyncSession
@@ -29,90 +30,92 @@ class SQLAlchemyProps:
         without negatively impacting static analysis (e.g. typing, code completion).
     """
 
-    alias = sa.alias
-    all_ = sa.all_
-    and_ = sa.and_
-    any_ = sa.any_
-    asc = sa.asc
-    between = sa.between
-    BigInteger = sa.BigInteger
-    bindparam = sa.bindparam
-    BLANK_SCHEMA = sa.BLANK_SCHEMA
-    Boolean = sa.Boolean
-    case = sa.case
-    cast = sa.cast
-    CheckConstraint = sa.CheckConstraint
-    collate = sa.collate
-    column = sa.column
-    Column = sa.Column
-    ColumnDefault = sa.ColumnDefault
-    Constraint = sa.Constraint
-    Date = sa.Date
-    DateTime = sa.DateTime
-    DDL = sa.DDL
-    DefaultClause = sa.DefaultClause
-    delete = sa.delete
-    desc = sa.desc
-    distinct = sa.distinct
-    Enum = sa.Enum
-    except_ = sa.except_
-    except_all = sa.except_all
-    exists = sa.exists
-    extract = sa.extract
-    false = sa.false
-    FetchedValue = sa.FetchedValue
-    Float = sa.Float
-    ForeignKey = sa.ForeignKey
-    ForeignKeyConstraint = sa.ForeignKeyConstraint
-    func = sa.func
-    funcfilter = sa.funcfilter
-    Index = sa.Index
-    insert = sa.insert
-    inspect = sa.inspect
-    Integer = sa.Integer
-    intersect = sa.intersect
-    intersect_all = sa.intersect_all
-    Interval = sa.Interval
-    join = sa.join
-    JSON = sa.JSON
-    LargeBinary = sa.LargeBinary
-    lateral = sa.lateral
-    literal = sa.literal
-    literal_column = sa.literal_column
-    MetaData = sa.MetaData
-    modifier = sa.modifier
-    not_ = sa.not_
-    null = sa.null
-    Numeric = sa.Numeric
-    or_ = sa.or_
-    outerjoin = sa.outerjoin
-    outparam = sa.outparam
-    over = sa.over
-    PickleType = sa.PickleType
-    PrimaryKeyConstraint = sa.PrimaryKeyConstraint
-    select = sa.select
-    Sequence = sa.Sequence
-    SmallInteger = sa.SmallInteger
-    String = sa.String
-    subquery = sa.subquery
-    table = sa.table
-    Table = sa.Table
-    tablesample = sa.tablesample
-    text = sa.text
-    Text = sa.Text
-    ThreadLocalMetaData = sa.ThreadLocalMetaData
-    Time = sa.Time
-    true = sa.true
-    tuple_ = sa.tuple_
-    type_coerce = sa.type_coerce
-    TypeDecorator = sa.TypeDecorator
-    Unicode = sa.Unicode
-    UnicodeText = sa.UnicodeText
-    union = sa.union
-    union_all = sa.union_all
-    UniqueConstraint = sa.UniqueConstraint
-    update = sa.update
-    within_group = sa.within_group
+    def __init__(self):
+        self.Row = Row
+        self.alias = sa.alias
+        self.all_ = sa.all_
+        self.and_ = sa.and_
+        self.any_ = sa.any_
+        self.asc = sa.asc
+        self.between = sa.between
+        self.BigInteger = sa.BigInteger
+        self.bindparam = sa.bindparam
+        self.BLANK_SCHEMA = sa.BLANK_SCHEMA
+        self.Boolean = sa.Boolean
+        self.case = sa.case
+        self.cast = sa.cast
+        self.CheckConstraint = sa.CheckConstraint
+        self.collate = sa.collate
+        self.column = sa.column
+        self.Column = sa.Column
+        self.ColumnDefault = sa.ColumnDefault
+        self.Constraint = sa.Constraint
+        self.Date = sa.Date
+        self.DateTime = sa.DateTime
+        self.DDL = sa.DDL
+        self.DefaultClause = sa.DefaultClause
+        self.delete = sa.delete
+        self.desc = sa.desc
+        self.distinct = sa.distinct
+        self.Enum = sa.Enum
+        self.except_ = sa.except_
+        self.except_all = sa.except_all
+        self.exists = sa.exists
+        self.extract = sa.extract
+        self.false = sa.false
+        self.FetchedValue = sa.FetchedValue
+        self.Float = sa.Float
+        self.ForeignKey = sa.ForeignKey
+        self.ForeignKeyConstraint = sa.ForeignKeyConstraint
+        self.func = sa.func
+        self.funcfilter = sa.funcfilter
+        self.Index = sa.Index
+        self.insert = sa.insert
+        self.inspect = sa.inspect
+        self.Integer = sa.Integer
+        self.intersect = sa.intersect
+        self.intersect_all = sa.intersect_all
+        self.Interval = sa.Interval
+        self.join = sa.join
+        self.JSON = sa.JSON
+        self.LargeBinary = sa.LargeBinary
+        self.lateral = sa.lateral
+        self.literal = sa.literal
+        self.literal_column = sa.literal_column
+        self.MetaData = sa.MetaData
+        self.modifier = sa.modifier
+        self.not_ = sa.not_
+        self.null = sa.null
+        self.Numeric = sa.Numeric
+        self.or_ = sa.or_
+        self.outerjoin = sa.outerjoin
+        self.outparam = sa.outparam
+        self.over = sa.over
+        self.PickleType = sa.PickleType
+        self.PrimaryKeyConstraint = sa.PrimaryKeyConstraint
+        self.select = sa.select
+        self.Sequence = sa.Sequence
+        self.SmallInteger = sa.SmallInteger
+        self.String = sa.String
+        self.subquery = sa.subquery
+        self.table = sa.table
+        self.Table = sa.Table
+        self.tablesample = sa.tablesample
+        self.text = sa.text
+        self.Text = sa.Text
+        self.ThreadLocalMetaData = sa.ThreadLocalMetaData
+        self.Time = sa.Time
+        self.true = sa.true
+        self.tuple_ = sa.tuple_
+        self.type_coerce = sa.type_coerce
+        self.TypeDecorator = sa.TypeDecorator
+        self.Unicode = sa.Unicode
+        self.UnicodeText = sa.UnicodeText
+        self.union = sa.union
+        self.union_all = sa.union_all
+        self.UniqueConstraint = sa.UniqueConstraint
+        self.update = sa.update
+        self.within_group = sa.within_group
 
 
 class Database(SQLAlchemyProps):
@@ -127,6 +130,7 @@ class Database(SQLAlchemyProps):
         app=None,
         #
     ):
+        super().__init__()
         self.engine = create_async_engine(conf.DATABASE_CONFIG.url, echo=True)
         self.session_factory = sessionmaker(
             bind=self.engine, class_=AsyncSession, future=True
